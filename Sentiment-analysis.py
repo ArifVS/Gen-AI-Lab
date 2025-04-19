036e50f171088cbdb48fadf5eae1d4d162023e4e
Arif <v.s.arifulla@gmail.com>
2025-04-19 15:15:49 +0530

    Update Sentiment-analysis.py

--- Sentiment-analysis.py
+++ Sentiment-analysis.py
@@ -1,1 +1,6 @@
-
+!pip install transformers
+from transformers import pipeline
+Classifier = pipeline("sentiment-analysis")
+user_input = input("Enter a sentence:")
+result = Classifier(user_input)
+print(result)
